d4d727cb76c8d376c0fadc8e3dca343070da1b90
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-05-03 00:26:07 +0200

    update

--- src/components/ChatView.vue
+++ src/components/ChatView.vue
@@ -70,28 +70,16 @@ const onMessage = (el, done) => {
 
 <footer class="chat-input">
   <form class="chat-form" @submit.prevent="onSubmit">
-    <button class="icon" @click="toggleChatEmotesOpen">
-      <BaseIcon name="add_reaction" />
-      <div>
-        <template v-for="(item, index) in chatEmoteList" :key="index">
-          <input type="radio" :id="item.name" :value="item.name" v-model="chatEmote" />
-          <label :for="item.name">
+    <button class="icon icon-select-button">
+      <BaseIcon name="add_reaction" @click="toggleChatEmotesOpen()" />
+      <div class="icon-select-group" :class="{ open: chatEmotesOpen }">
+        <div class="icon-select"   v-for="(item, index) in chatEmoteList" :key="index">
+          <input type="radio" :id="index" :value="item.name" v-model="chatEmote" />
+          <label :for="index">
             <BaseIcon :name="item.name" />
           </label>
-
-
-
-
-        </template>
+        </div>
       </div>
-      <input type="radio" id="sentiment_satisfied" value="sentiment_satisfied" v-model="chatEmote" />
-      <label for="sentiment_satisfied">
-        <BaseIcon name="sentiment_satisfied" />
-      </label>
-      <input type="radio" id="mood_bad" value="mood_bad" v-model="chatEmote" />
-      <label for="mood_bad">
-        <BaseIcon name="mood_bad" />
-      </label>
     </button>
     <input class="message-input" type="textarea" v-model="message" required placeholder="your message" />
     <button class="icon">
@@ -112,6 +100,76 @@ const onMessage = (el, done) => {
   opacity: 0;
   transform: translateY(30px);
 }
+
+
+button.icon-select-button {
+  & > span {
+    z-index: 3;
+    background: #000;
+  }
+}
+
+.icon-select-group {
+  font-size: 1.4em;
+  position: absolute;
+  bottom: 0em;
+  transition: all 300ms;
+  
+
+
+  &.open {
+    bottom: 2em;
+    position: absolute;
+    gap: 1em;
+    z-index: 100;
+
+    .icon-select{
+      margin-bottom: 1em;
+    }
+    
+  }
+}
+
+
+
+
+.icon-select {
+  background: red;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: .25em;
+  border-radius: 100%;
+  width: 1.5em;
+  height: 1.5em;
+  
+
+  label {
+    // display: flex;
+    display: inline-block;
+    position: relative;
+    width: 1.5em;
+    height: 1.5em;
+    margin: auto;
+  }
+
+  input[type=radio] {
+    width: 1.5em;
+    height: 1.5em;
+    display: inline-block;
+    position: absolute;
+    z-index: 3;
+    appearance: none;
+    
+    &:checked {
+      height: 5em;
+    }
+  }
+}
+
+
+
 
 
 main {
