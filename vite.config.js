1f7a0a1715f75d415fe3dfc31627cd17dd1cf124
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-05-10 19:25:41 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/human-centered-design/',
+  base: '/human-centered-design',
   plugins: [vue()],
   css: {
     devSourcemap: true
