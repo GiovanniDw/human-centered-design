cd28fb3e4e904d01ec6a283a76ef5643ce7be6c9
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-21 17:17:03 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '.',
+  base: '/human-centered-design',
   plugins: [vue()],
   css: {
     devSourcemap: true
